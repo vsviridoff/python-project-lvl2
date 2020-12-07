5f4d0808bd8840fa3dda60ff987b94562c402841
Vladislav Sviridov <v.sviridov.dev@gmail.com>
2020-12-07 22:49:27 +0300

    Add testing yaml/yml files

--- tests/test_flat_format.py
+++ tests/test_flat_format.py
@@ -1,10 +1,17 @@
 import gendiff.gendiff as gd
 
 
+ERROR_MASAGE = "Flat diffs are not the same"
+
+
 def test_flat():
     with open('./tests/fixtures/expexted_flat.txt', 'r') as input_file:
         expected = input_file.read()
-    assert expected == gd.create_diff(
+    assert expected == gd.generate_diff(
         './tests/fixtures/first_file.json',
         './tests/fixtures/second_file.json'
-    ), "Flat diffs are not the same"
+    ), ERROR_MASAGE
+    assert expected == gd.generate_diff(
+        './tests/fixtures/first_file.yaml',
+        './tests/fixtures/second_file.yaml'
+    ), ERROR_MASAGE
