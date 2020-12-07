14a7d044218e8de462da982027eddd3e8c1f0fae
Vladislav Sviridov <v.sviridov.dev@gmail.com>
2020-12-07 22:50:38 +0300

    Make new modele file_parcer.py

---
+++ gendiff/file_parcer.py
@@ -0,0 +1,12 @@
+import json
+import yaml
+import os
+
+
+def get_file_data(file):
+    _, file_extentions = os.path.splitext(file)
+    if file_extentions == '.json':
+        data = json.load(open(file))
+    elif file_extentions == '.yaml' or file_extentions == '.yml':
+        data = yaml.safe_load(open(file))
+    return data
