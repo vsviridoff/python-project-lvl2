f06026fc387eb90331763d435d2dc231c49cf3a9
Vladislav Sviridov <v.sviridov.dev@gmail.com>
2021-01-26 20:23:49 +0300

    Add support for plain format

--- gendiff/formatter.py
+++ gendiff/formatter.py
@@ -1,9 +1,11 @@
-from gendiff.formats import stylish
+from gendiff.formats import stylish, plain
 
 
 def get_format(output_format):
     if output_format == 'stylish':
         format = stylish
+    elif output_format == 'plain':
+        format = plain
     else:
         raise ValueError('Invalid output format')
 
