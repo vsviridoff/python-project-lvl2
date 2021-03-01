3513f194e0ad2b4faf409019fea2bee5665e0fda
Vladislav Sviridov <v.sviridov.dev@gmail.com>
2021-03-01 20:55:06 +0300

    Add file cli.py

---
+++ gendiff/cli.py
@@ -0,0 +1,15 @@
+import argparse
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument(
+        '-f', '--format',
+        default='stylish',
+        help='set format of output (stylish, json, plain)\ndefault: stylish',
+        type='str'
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+
+    return parser
